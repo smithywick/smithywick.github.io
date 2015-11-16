625782aaceb995993f6697787ecea14e58c24ad8
smithywick <ccsmith@mtu.edu>
2015-11-16 16:59:35 -0600

    Update bbdata.js

--- js/services/bbdata.js
+++ js/services/bbdata.js
@@ -1,3 +1,9 @@
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
+
 app.factory('bbdata', ['$http', function($http) { 
   return $http.get('http://www.bestbuy.com/api/1.0/product/summaries?skus=1107008,1581921&includeInactive=false') 
             .success(function(data) { 
