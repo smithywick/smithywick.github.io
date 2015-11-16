a9e5120745e52b4bdc19805af0015c2870e2f4be
smithywick <ccsmith@mtu.edu>
2015-11-16 16:11:13 -0600

    Actual Images

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,25 +1,25 @@
 app.controller('mainController', ['$scope', function($scope) {
   $scope.apps = [ 
 	  { 
-	    icon: 'img/move.jpg', 
+	    icon: 'http://pisces.bbystatic.com/image2/BestBuy_US/images/products/4221/4221508_sa.jpg', 
 	    title: 'MOVE', 
 	    developer: 'MOVE, Inc.', 
 	    price: 0.99 
 	  }, 
 	  { 
-	    icon: 'img/shutterbugg.jpg', 
+	    icon: 'http://pisces.bbystatic.com/image2/BestBuy_US/images/products/4221/4221508_sa.jpg', 
 	    title: 'Shutterbugg', 
 	    developer: 'Chico Dusty', 
 	    price: 2.99 
 	  },
 	  {
-	    icon: 'img/gameboard.jpg',
+	    icon: 'http://pisces.bbystatic.com/image2/BestBuy_US/images/products/4221/4221508_sa.jpg',
 	    title: 'Gameboard',
 	    developer: 'Armando P.',
 	    price: 1.99
 	  },
 	  {
-	    icon: 'img/forecast.jpg',
+	    icon: 'http://pisces.bbystatic.com/image2/BestBuy_US/images/products/4221/4221508_sa.jpg',
 	    title: 'Forecast',
 	    developer: 'Forecast',
 	    price: 1.99
