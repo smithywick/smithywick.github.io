dc1cc0af5a9ad2995ca546fc963fcdae01e322e5
smithywick <ccsmith@mtu.edu>
2016-04-27 22:00:31 -0500

    Initial commit

---
+++ es6/src/estest.js
@@ -0,0 +1,15 @@
+export default class Person {
+
+  constructor( name ) {
+    this.name = name;
+  }
+
+  sayHello() {
+    return `Hello ${ this.name }!`;
+  }
+
+  sayHelloThreeTimes() {
+    let hello = this.sayHello();
+    return `${ hello } `.repeat(3);
+  }
+}
