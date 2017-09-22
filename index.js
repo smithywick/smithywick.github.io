c081c9a5ebfb2dcf3e40e3e2e8ff82e322a7d065
chadsmith <Chad.Smith2@target.com>
2017-09-22 11:54:04 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello Jake!");
 
 });
 
