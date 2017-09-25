e1502f575d4f1f51c7d1f09ed84472f3259fc172
chadsmith <Chad.Smith2@target.com>
2017-09-25 14:27:02 -0500

    updated all

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 
-var server = http.createServer(function (request, response) {
-
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello Azure!");
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
