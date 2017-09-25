e813c7d1674fca28c93844c6fb18416c2745c3ce
chadsmith <Chad.Smith2@target.com>
2017-09-25 12:45:24 -0500

    updated all

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListItem from './listitem';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class App extends Component {
+  constructor() {
+    super();
+    this.updateData = this.updateData.bind(this);
+
+    this.state = {
+      data: false
+    }
+  }
+  updateData() {
+    const url = 'something';
+    fetch(
+      url, {
+        url: url,
+        headers: {
+          Accept: 'application/json;odata=verbose',
+        },
+        dataType: 'json',
+        contentType: 'application/json;odata=verbose',
+        method: 'GET',
+        credentials: 'same-origin',
+      },
+    ).then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        const error = new Error(response.statusText);
+        error.status = response.status;
+        console.log(error);
+      }
+      return response.json();
+    })
+      .then((data) => {
+        this.setState({
+          data: data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="App" >
+        <div className="list">
+          <div className="button" onclick={this.updateData}>button </div>
+          <div className="list">
+            {
+              this.state.data
+              &&
+              this.state.data.map(
+                (item) =>
+                  <ListItem key={item.id} className="listItem " data={item} />
+              )
+            }
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
